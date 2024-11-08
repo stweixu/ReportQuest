3c018c95c24acbb4002e73b188047ea2b8816062
maxwell <maxweliau123456@gmail.com>
2024-11-08 13:18:17 +0800

    pooped my pants twice

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://127.0.0.1",
+    "http://127.0.0.1/",
     "http://127.0.0.1:5173/",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173/",
     "http://localhost:5173",
 ]
 
