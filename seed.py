b87e19875518f755413a6e181c4ced9dfb64d787
maxwell <maxweliau123456@gmail.com>
2024-11-13 00:25:24 +0800

    reordered seedinm

--- seed.py
+++ seed.py
@@ -7,6 +7,15 @@ import os
 base_path = "database_seeder"
 
 for i in os.listdir(base_path):
+    if i == "seedAuthority.py":
+        continue
     command = f"python {base_path}/{i}"
     print(f"executing {command}")
     os.system(command)
+
+# seed authority AFTER user
+
+command = f"python {base_path}/seedAuthority.py"
+print(f"executing {command}")
+
+print("done seeding")
